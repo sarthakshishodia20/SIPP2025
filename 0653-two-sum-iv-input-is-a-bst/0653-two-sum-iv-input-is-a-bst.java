00c7e098730016d798fccf0ade314d9dd654aa7e
SARTHAK SHISHODIA <122453870+sarthakshishodia20@users.noreply.github.com>
2025-07-20 12:19:34 +0530

    Time: 3 ms (62.89%), Space: 45.5 MB (20.17%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -1,34 +1,72 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public boolean findTarget(TreeNode root, int k) {
-        HashSet<Integer> set=new HashSet<>();
-        return inorder(root,set,k);
+    static class BSTIterator {
+        private Stack<TreeNode> s = new Stack<>();
+        public BSTIterator(TreeNode root) {
+            pushAllLeft(root);
+        }
+        private void pushAllLeft(TreeNode node) {
+            while (node != null) {
+                s.push(node);
+                node = node.left;
+            }
+        }
+        public int next() {
+            TreeNode top = s.pop();
+            if (top.right != null) {
+                pushAllLeft(top.right);
+            }
+            return top.val;
+        }
+        public boolean hasNext() {
+            return !s.isEmpty();
+        }
     }
-    public static boolean inorder(TreeNode root,HashSet<Integer> set,int k){
-        if(root==null){
-            return false;
+    static class BSTReverseIterator {
+        private Stack<TreeNode> s = new Stack<>();
+        public BSTReverseIterator(TreeNode root) {
+            pushAllRight(root);
         }
-        if(inorder(root.left,set,k)){
-            return true;
+        private void pushAllRight(TreeNode node) {
+            while (node != null) {
+                s.push(node);
+                node = node.right;
+            }
         }
-        if(set.contains(k-root.val)){
-            return true;
+        public int prev() {
+            TreeNode top = s.pop();
+            if (top.left != null) {
+                pushAllRight(top.left);
+            }
+            return top.val;
         }
-        set.add(root.val);
-        return inorder(root.right,set,k);
+        public boolean hasPrev() {
+            return !s.isEmpty();
+        }
+    }
+    public boolean findTarget(TreeNode root, int k) {
+        if (root == null) return false;
+        BSTIterator l = new BSTIterator(root);
+        BSTReverseIterator r = new BSTReverseIterator(root);
+        int i = l.next(); 
+        int j = r.prev(); 
+        while (i < j) {
+            int sum = i + j;
+            if (sum == k) {
+                return true;
+            } else if (sum < k) {
+                if (l.hasNext()) {
+                    i = l.next();
+                } else {
+                    return false;
+                }
+            } else {
+                if (r.hasPrev()) {
+                    j = r.prev();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return false;
     }
 }
