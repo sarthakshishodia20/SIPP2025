89c56128ad6ad4a3daa4176f892bca3f563dfd79
SARTHAK SHISHODIA <122453870+sarthakshishodia20@users.noreply.github.com>
2025-08-24 07:02:11 +0530

    Time: 3 ms (77.92%), Space: 56.9 MB (61.05%) - LeetHub

---
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int longestSubarray(int[] nums) {
+        int i=0;
+        int j=0;
+        int n=nums.length;
+        int k=1;
+        int zeroCount=0;
+        int maxSize=0;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        while(j<n){
+            if(nums[j]==0){
+                zeroCount++;
+            }
+            while(zeroCount>k){
+                if(nums[i]==0){
+                    zeroCount--;
+                }
+                i++;
+            }
+            if(zeroCount<=k){
+                maxSize=Math.max(maxSize,(j-i+1));
+            }
+            j++;
+        }
+        return maxSize-1;
+    }
+}
